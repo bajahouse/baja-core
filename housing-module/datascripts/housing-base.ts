20fe5f76c040a535746a903c192aa6fc52821f20
tester <connorkilbourne@gmail.com>
2022-04-29 15:55:48 -0400

    add in sql creation in datascripts

--- housing-module/datascripts/housing-base.ts
+++ housing-module/datascripts/housing-base.ts
@@ -2,6 +2,8 @@ import { std } from "wow/wotlk";
 
 export const areaGroupID = 3000;
 std.DBC.AreaGroup.add(areaGroupID).AreaID.set([1977]); //change to a dungeon eventually
+std.SQL.Databases.world_dest.writeEarly('DROP TABLE IF EXISTS `player_housing_spell_object_link`;')
+std.SQL.Databases.world_dest.writeEarly('CREATE TABLE `player_housing_spell_object_link` (`spellID` int(11) NULL DEFAULT NULL, `typeID` int(11) NULL DEFAULT NULL, `entry` int(11) NULL DEFAULT NULL)')
 
-//create sql table  player_housing_spell_object_link(world)(links spellID to gobID, should be in memory on startup)(each time truncate and go again(possibly just use REPLACE INTO isntead of truncate?)) and
-// player_housing(characters)(once) stores each players instance of gobs/creatures/versioniD
+//execute this in your characters/ DB 
+//CREATE TABLE IF NOT EXISTS `player_housing` ( `playerGUID` int(11) NOT NULL, `mapID` int(11) NULL DEFAULT NULL, `versionID` int(11) NULL DEFAULT NULL, `typeID` int(11) NULL DEFAULT NULL, `entry` int(11) NULL DEFAULT NULL, `x` float NULL DEFAULT NULL, `y` float NULL DEFAULT NULL, `z` float NULL DEFAULT NULL, `o` float NULL DEFAULT NULL )
