92f0217a5fe9f3db019a131a51c9ed596fa54fe7
tester <connorkilbourne@gmail.com>
2022-04-29 15:56:10 -0400

    updating for different sql table

--- housing-module/livescripts/player_housing.ts
+++ housing-module/livescripts/player_housing.ts
@@ -4,11 +4,8 @@ export function housing(events: TSEvents) {
         if (!map.GetBool("isSpawned", false)) {
             map.SetBool("isSpawned", true);
             map.SetUInt("playerOwner", player.GetGUIDLow());
-            player.SendAreaTriggerMessage("Welcome home, " + player.GetName() + "!");
-            let q = QueryCharacters("SELECT * FROM `player_housing` WHERE guid = " + player.GetGUIDLow());
-            while (q.GetRow()) {
-                map.SpawnGameObject(q.GetInt32(1), q.GetFloat(2), q.GetFloat(3), q.GetFloat(4), q.GetFloat(5));
-            }
+            player.SendAreaTriggerMessage("Welcome home, " + player.GetName() + "! You are displaying version " + 1);
+            spawnMap(map, player, 1)
         }
     });
 
@@ -21,17 +18,19 @@ export function housing(events: TSEvents) {
     });
 
     events.SpellID.OnCheckCast(GetIDTag('housing-mod', 'spawn-gob'), (spell, result) => {
+        if (!spell.GetCaster().IsPlayer())
+            return
+
         let player = spell.GetCaster().ToPlayer();
         if (player.GetMap().GetUInt("playerOwner", 1) != player.GetGUIDLow()) {
             player.SendAreaTriggerMessage("This is not your home!");
             spell.Cancel();
-        } else {
-            //eventually replace with a dictionary map that gets made at start of server
-            let q = QueryWorld("SELECT entry,typeID FROM `player_housing_spell_object_link` WHERE spellID = " + spell.GetEntry());
-            while (q.GetRow()) {
-                QueryCharacters("INSERT INTO `player_housing` VALUES(" + player.GetGUIDLow() + "," + q.GetUInt32(0) + "," + q.GetUInt32(1) + "," + spell.GetTargetDest().x + "," + spell.GetTargetDest().y + "," + spell.GetTargetDest().z + "," + spell.GetTargetDest().o + ")"
-                );
-            }
+            return
+        }
+        //eventually replace with a dictionary map that gets made at start of server
+        let q = QueryWorld("SELECT typeID,entry FROM `player_housing_spell_object_link` WHERE spellID = " + spell.GetEntry());
+        while (q.GetRow()) {
+            QueryCharacters("INSERT INTO `player_housing` VALUES(" + player.GetGUIDLow() + "," + player.GetMapID()+ "," + player.GetMap().GetUInt('versionID',1) + "," + q.GetUInt32(0) + "," + q.GetUInt32(1) + "," + spell.GetTargetDest().x + "," + spell.GetTargetDest().y + "," + spell.GetTargetDest().z + "," + spell.GetTargetDest().o + ")");
         }
     })
 
@@ -45,17 +44,18 @@ export function housing(events: TSEvents) {
         }
         let gobs = player.GetGameObjectsInRange(10, 0, 0);
         player.GossipClearMenu();
-        if (gobs.length > 0) {
-            if (gobs.length > 32) {
-                gobs = gobs.slice(0, 31);
-            }
-            for (let i = 0; i < gobs.length; i++) {
-                player.GossipMenuAddItem(0, "Remove " + gobs[i].GetName() + " ID:" + gobs[i].GetGUIDLow(), 0,
-                    gobs[i].GetGUIDLow(), false,
-                    "", 0);
-            }
-            player.GossipSendMenu(5, gobs[0], 0);
+        if (gobs.length == 0) {
+            player.SendAreaTriggerMessage('Unable to find any objects!')
+            return
         }
+        if (gobs.length > 32) {
+            gobs = gobs.slice(0, 31);
+        }
+        for (let i = 0; i < gobs.length; i++) {
+            player.GossipMenuAddItem(0, "Remove " + gobs[i].GetName() + " Entry:" + gobs[i].GetEntry(), 0, i);//, false, "", 0
+        }
+        player.GossipSendMenu(5, gobs[0], 0);
+
     });
 
     events.GameObjects.OnGossipSelect((obj, player, menuID, selection, cancel) => {
@@ -64,22 +64,20 @@ export function housing(events: TSEvents) {
             player.SendAreaTriggerMessage("This is not your home!");
             return
         }
-
         let gobs: TSArray<TSGameObject> = player.GetGameObjectsInRange(10, 0, 0);
-        let index = -1;
-        for (let i = 0; i < gobs.length; i++) {
-            if (gobs[i].GetGUIDLow() == selection) {
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) {
-            player.SendAreaTriggerMessage("Unable to find that game object!");
-        } else {
-            let pos = gobs[index].GetPosition();
-            gobs[index].Despawn();
-            QueryCharacters("DELETE FROM `player_housing` WHERE x = " + pos.x + " AND y = " + pos.y + " AND z = " + pos.z + ";"
-            );
-        }
+        let pos = gobs[selection].GetPosition();
+
+        QueryCharacters("DELETE FROM `player_housing` WHERE playerGUID = " + player.GetGUIDLow() + " AND mapID = " + player.GetMapID() + " AND versionID = " + player.GetMap().GetUInt('versionID', 1) + "AND entry = " + gobs[selection].GetEntry() + " AND x = " + pos.x + " AND y = " + pos.y + " AND z = " + pos.z +  " AND o = " + pos.o + ";");
+        gobs[selection].Despawn();
     });
 }
+function spawnMap(map: TSMap, player: TSPlayer, versionID: uint32) {
+    let q = QueryCharacters("SELECT typeID,entry,x,y,z,o FROM `player_housing` WHERE playerGUID = " + player.GetGUIDLow() + " AND mapID = " + map.GetMapID() + " AND versionID = " + versionID);
+    while (q.GetRow()) {
+        if (q.GetInt32(0) == 0)
+            map.SpawnGameObject(q.GetInt32(1), q.GetFloat(2), q.GetFloat(3), q.GetFloat(4), q.GetFloat(5));
+        else if (q.GetInt32(0) == 1)
+            map.SpawnCreature(q.GetInt32(1), q.GetFloat(2), q.GetFloat(3), q.GetFloat(4), q.GetFloat(5))
+    }
+}
+
