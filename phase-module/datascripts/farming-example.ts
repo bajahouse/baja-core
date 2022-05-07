18f0dc1a11c2bd5ed25d374cc1618aa6947d44b7
tester <connorkilbourne@gmail.com>
2022-05-06 21:51:58 -0400

    spacing

--- phase-module/datascripts/farming-example.ts
+++ phase-module/datascripts/farming-example.ts
@@ -9,6 +9,6 @@ makeHousingItemForGob(10192, "Wooden Chair")
 makeHousingItemForCreature(2671, "Squirrel")
 
 let rewardHarvest = std.Items.create(MODNAME, "farming-reward-harvest", 738);
-makeHousingItemForCrop(188667,188691,'Wheat',10000,rewardHarvest.ID,1,3)
+makeHousingItemForCrop(188667, 188691, 'Wheat', 10000, rewardHarvest.ID, 1, 3)
 
-makeHousingItemForFertilizer('Water',13)//multiply by 10, 1.3->13
+makeHousingItemForFertilizer('Water', 13)//multiply by 10, 1.3->13
