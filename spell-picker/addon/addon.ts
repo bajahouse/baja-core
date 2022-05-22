e8346fd660b583a483aa73f424b365cc2a86d686
nl <nldev00@pm.me>
2022-05-21 21:47:23 -0500

    working addon example

--- spell-picker/addon/addon.ts
+++ spell-picker/addon/addon.ts
@@ -1,48 +1,37 @@
-import { $, Addon } from './lib/lib'
-import { Grid } from './lib/frames/grid'
-import { Scroll } from './lib/frames/scroll'
+import { $, Addon, Movable } from './baja/lib'
+import { Grid } from './baja/frames/grid'
+import { Scroll } from './baja/frames/scroll'
 
 Addon('spell-picker', () => {
-  $({
+  const picker = $({
+    uid: 'spell-picker',
     width: 500,
     height: 500,
-    backdrop: 'tutorial',
+    backdrop: 'tooltip',
+    point: 'CENTER',
   })
+
+  Movable(picker, 'RightButton')
+
+  const scroll = Scroll({
+    scrollHeight: 1500,
+    width: 450,
+    height: 450,
+    parent: picker,
+  })
+
+  const grid = Grid({
+    itemsPerRow: 4,
+    rowHeight: 100,
+    parent: scroll,
+  })
+
+  const foo = $({
+    backdrop: 'tooltip',
+    color: [1, 0, 0],
+    width: 50,
+    height: 50,
+  })
+
+  grid.Attach(foo)
 })
-
-
-const frame = CreateFrame('Frame')
-frame.SetSize(500, 500)
-frame.SetBackdrop({
-  insets: { top: 4, right: 4, bottom: 4, left: 4 },
-  tile: true,
-  tileSize: 16,
-  edgeSize: 16,
-  bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
-  edgeFile: '',
-})
-frame.SetParent(UIParent)
-frame.SetPoint('CENTER')
-
-// Addon('spell-picker', () => {
-//   const picker = $({
-//     uid: 'spell-picker',
-//     width: 500,
-//     height: 500,
-//     backdrop: 'tooltip',
-//   })
-
-//   const scroll = Scroll({
-//     scrollHeight: 1500,
-//     width: 450,
-//     height: 450,
-//     parent: picker,
-//   })
-
-//   const grid = Grid({
-//     itemsPerRow: 4,
-//     rowHeight: 100,
-//     parent: scroll,
-//   })
-// })
-
