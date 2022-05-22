5e1139a87a7a1db8c31069e7294c76e35127d729
nl <nldev00@pm.me>
2022-05-21 21:15:45 -0500

    clean spell picker

--- spell-picker/addon/addon.ts
+++ spell-picker/addon/addon.ts
@@ -1,7 +1,7 @@
 
-import { Grid } from './lib/frames/grid'
-import { Scroll } from './lib/frames/scroll'
 import { $, Addon } from './lib/lib'
+import { Grid } from './lib/frames/grid'
+
 
 Addon('spell-picker', () => {
   const picker = $({
@@ -9,19 +9,6 @@ Addon('spell-picker', () => {
     width: 500,
     height: 500,
     backdrop: 'tooltip',
-  })
-
-  const scroll = Scroll({
-    scrollHeight: 1500,
-    pctWidth: 90,
-    pctHeight: 90,
-    parent: picker,
-  })
-
-  const grid = Grid({
-    itemsPerRow: 4,
-    rowHeight: 100,
-    parent: scroll,
   })
 })
 
