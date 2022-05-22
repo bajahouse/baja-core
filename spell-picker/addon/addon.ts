a6ba35ad01db55a894b9acc216814f9d4b1cad34
nl <nldev00@pm.me>
2022-05-21 20:11:36 -0500

    spell picker scroll + grid

--- spell-picker/addon/addon.ts
+++ spell-picker/addon/addon.ts
@@ -1,11 +1,27 @@
+
+import { Grid } from './lib/frames/grid'
+import { Scroll } from './lib/frames/scroll'
 import { $, Addon } from './lib/lib'
 
-Addon('spell-picker', app => {
-  const main = $({
+Addon('spell-picker', () => {
+  const picker = $({
     uid: 'spell-picker',
     width: 500,
     height: 500,
     backdrop: 'tooltip',
+  })
+
+  const scroll = Scroll({
+    scrollHeight: 1500,
+    pctWidth: 90,
+    pctHeight: 90,
+    parent: picker,
+  })
+
+  const grid = Grid({
+    itemsPerRow: 4,
+    rowHeight: 100,
+    parent: scroll,
   })
 })
 
