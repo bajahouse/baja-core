e34aa94395495ece9cc5bb2d1ced429ae6048091
tester <connorkilbourne@gmail.com>
2022-05-23 16:30:14 -0400

    correct sql stuff

--- infinite-dungeon-module/datascripts/database-setup.ts
+++ infinite-dungeon-module/datascripts/database-setup.ts
@@ -1,6 +1,14 @@
 import { std } from "wow/wotlk";
 
 //dungeon
-std.SQL.Databases.world_dest.writeEarly('DROP TABLE IF EXISTS `dungeon_spells`;')
-std.SQL.Databases.world_dest.writeEarly('CREATE TABLE `dungeon_spells`  (  `classID` int(11) NOT NULL,  `spellID` int(11) NOT NULL,  `rarity` int(11) NULL DEFAULT NULL,  `type` int(11) NULL DEFAULT NULL,  `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,  PRIMARY KEY (`classID`, `spellID`) USING BTREE)')
+std.SQL.Databases.world_dest.writeEarly(`
+DROP TABLE IF EXISTS \`dungeon_spells\`;
+CREATE TABLE \`dungeon_spells\`  (
+  \`classID\` int(11) NULL DEFAULT NULL,
+  \`spellID\` int(11) NULL DEFAULT NULL,
+  \`spellRarity\` int(11) NULL DEFAULT NULL,
+  \`spellType\` int(11) NULL DEFAULT NULL,
+  \`spellDesc\` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+`)
 
