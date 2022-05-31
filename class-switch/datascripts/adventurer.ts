303c9e8afa4192ccdd39e0b75322b36757b4e56c
tester <connorkilbourne@gmail.com>
2022-05-30 20:49:48 -0400

    aquiremethod to solve autolearn issue

--- class-switch/datascripts/adventurer.ts
+++ class-switch/datascripts/adventurer.ts
@@ -30,7 +30,7 @@ std.SkillLines.forEach((line) => {
 })
 
 std.DBC.SkillLineAbility.queryAll({ RaceMask: 0 }).forEach(v => {
-    if (v.ClassMask.get() != 0) {
+    if (v.ClassMask.get() != 0 && v.AcquireMethod.get() == 0) {
         v.ClassMask.mark(ADVENTURER.ID - 1)
         v.RaceMask.markAll([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     }
