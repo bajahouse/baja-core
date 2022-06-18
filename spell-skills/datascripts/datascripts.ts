8a7d6e6b0e65207b9301cc9c56373e792adb5f75
nl <nldev00@pm.me>
2022-06-18 04:41:47 -0500

    spell skills more wip

--- spell-skills/datascripts/datascripts.ts
+++ spell-skills/datascripts/datascripts.ts
@@ -1,4 +1,6 @@
-import { DBC, std } from 'wow/wotlk'
+import { std } from 'wow/wotlk'
+import { SpellItemEnchantmentRow } from 'wow/wotlk/dbc/SpellItemEnchantment'
+import { Spell } from 'wow/wotlk/std/Spell/Spell'
 
 import { SPELLS } from './data'
 
@@ -6,7 +8,95 @@ SPELLS.filter(s => s.cls === 'ROGUE').forEach(s => {
 
 })
 
-let spell_skill_id = 9000
+let enchant_id = 9000
+
+function StackingEnchant (
+  mod: string,
+  id: string,
+  name: string,
+  tag: string,
+  amount: number,
+): [Spell, SpellItemEnchantmentRow] {
+  const dummy = std.Spells.create(mod, id, 7464)
+  const ench = std.DBC.SpellItemEnchantment.add(enchant_id++)
+  dummy.Stacks.set(0)
+  dummy.Power.setMana(0)
+  dummy.Range.set(0)
+  // dummy.Effects.addGet().Type.APPLY_AURA.set()
+  dummy.Effects.clearAll()
+  dummy.Effects.addGet().Type.APPLY_AURA.set().Aura.DUMMY.set()
+  dummy.Power.setMana(amount)
+  // dummy.Duration.set(0)
+  dummy.CastTime.set(0)
+  dummy.Name.enGB.set(name)
+  dummy.Tags.add('spell-skills', tag)
+  dummy.Description.enGB.set(name)
+  dummy.Name.enGB.set(name)
+  ench.Name.enGB.set(name)
+  ench.Effect.set([3, 0, 0])
+  ench.EffectPointsMin.set([0, 0, 0])
+  ench.EffectPointsMax.set([0, 0, 0])
+  ench.Flags.set(0)
+  ench.EffectArg.set([dummy.ID, 0, 0])
+  ench.ItemVisual.set(0)
+  // test
+  const item = std.Items.create('spell-skills', `DAGGER_${id}`, 2092)
+  item.Spells.addGet().Spell.set(dummy.ID).Trigger.set(1)
+  item.Name.enGB.set(`[TEST DAGGER] ${name}`)
+  return [dummy, ench]
+}
+
+const CLASSES = [
+  'Warrior',
+  'Rogue',
+  'Hunter',
+  'Mage',
+  'Priest',
+  'Warlock',
+  'Druid',
+  'Shaman',
+  'Paladin',
+  'Death Knight',
+]
+
+const SCHOOLS = [
+  'Fire',
+  'Frost',
+  'Nature',
+  'Shadow',
+  'Holy',
+  'Arcane',
+  'Physical',
+]
+
+const SPECIALIZATIONS = [
+  'Arms',
+  'Fury',
+  'Protection',
+  'Combat',
+  'Subtlety',
+  'Assassination',
+  'Marksmanship',
+  'Beast Mastery',
+  'Survival',
+  'Discipline',
+  'Restoration',
+  'Balance',
+  'Feral',
+  'Enhancement',
+  'Elemental',
+  'Affliction',
+  'Demonology',
+  'Destruction',
+  'Retribution',
+]
+
+const MISC = [
+  'All',
+  'Magic',
+  'Melee',
+  'Ranged',
+]
 
 SPELLS.forEach(spell => {
   spell.list.forEach(id => {
@@ -15,26 +105,30 @@ SPELLS.forEach(spell => {
   })
   for (let i = 1; i <= 10; i++) {
     const tag = spell.id + '_SKILL'
-    const id = spell.id + '_SKILL' + '_' + i
-    const dummy = std.Spells.create('spell-skills', id, 7529)
-    dummy.Stacks.set(255)
-    dummy.Effects.clearAll()
-    dummy.Effects.addGet().Type.APPLY_AURA.set().Aura.DUMMY.set()
+    const id = tag + '_' + i
     const name = `+${i} ${spell.english} Skill`
-    dummy.Tags.add('spell-skills', tag)
-    dummy.Name.enGB.set(name)
-    dummy.Description.enGB.set('')
-    const ench = std.DBC.SpellItemEnchantment.add(spell_skill_id++)
-    ench.Name.enGB.set(name)
-    ench.Effect.set([3, 0, 0])
-    ench.EffectPointsMin.set([0, 0, 0])
-    ench.EffectPointsMax.set([0, 0, 0])
-    ench.Flags.set(0)
-    ench.EffectArg.set([dummy.ID, 0, 0])
-    ench.ItemVisual.set(0)
-    if (spell.id === 'SINISTER_STRIKE')
-      console.log(dummy.objectify())
+    StackingEnchant('spell-skills', id, name, tag, i)
   }
   std.Spells.load(spell.first).Tags.addUnique('spell-skills', spell.id + '_FIRST')
   std.Spells.load(spell.last).Tags.addUnique('spell-skills', spell.id + '_LAST')
+})
+
+CLASSES.forEach(cls => {
+  for (let i = 1; i <= 10; i++)
+    StackingEnchant('spell-skills', cls + '_' + i, `+${i} ${cls} Skills`, `${cls}_SKILL`, i)
+})
+
+SCHOOLS.forEach(school => {
+  for (let i = 1; i <= 10; i++)
+    StackingEnchant('spell-skills', school + '_' + i, `+${i} ${school} Skills`, `${school}_SKILL`, i)
+})
+
+SPECIALIZATIONS.forEach(spec => {
+  for (let i = 1; i <= 10; i++)
+    StackingEnchant('spell-skills', spec + '_' + i, `+${i} ${spec} Skills`, `${spec}_SKILL`, i)
+})
+
+MISC.forEach(tag => {
+  for (let i = 1; i <= 10; i++)
+    StackingEnchant('spell-skills', tag + '_' + i, `+${i} ${tag} Skills`, `${tag}_SKILL`, i)
 })
--- spell-skills/livescripts/livescripts.ts
+++ spell-skills/livescripts/livescripts.ts
@@ -1,5 +1,58 @@
+// //requires beingfilled out
+// const skillToSpell = [
+//   [
+//     GetIDTag('spell-skills', 'SINISTER_STRIKE_SKILL'),
+//     GetIDTag('spell-skills', 'SINISTER_STRIKE'),
+//   ],
+//   [
+//     GetIDTag('spell-skills', 'FIREBALL_SKILL'),
+//     GetIDTag('spell-skills', 'FIREBALL'),
+//   ],
+// ]
 
-// on apply aura:
+// //generated
+// const miscToSpell = CreateDictionary<uint32, TSArray<uint32>>({})
+
+// export function M (events: TSEvents) {
+//   setupMiscToSpell()
+//   skillToSpell.forEach((val, i, arr) => {
+//     events.SpellID.OnApply(val[0], s => {
+//       const c = s.GetCaster()
+//       //bonus: it should apply this per-effect allowing you to mix/match bonus+negative
+//       //identifier: miscValueA. it should match to all other bonus sinisterstrike and be unique
+//       //bonus: MiscValueB. anything you want: +3, -6, etc
+//       const id = c.GetInt(s.GetMiscValue().toString(), 0) + (s.GetMiscValueB() || 0)
+//       const v: int32 = id
+//       c.SetInt(s.GetMiscValue().toString(), v)
+//     })
+//     events.SpellID.OnRemove(val[0], s => {
+//       const c = s.GetCaster()
+//       const v: int32 = c.GetInt(s.GetMiscValue().toString(), 0) - (s.GetMiscValueB() || 0)
+//       c.SetInt(s.GetMiscValue().toString(), v)            
+//     })
+//   })
+//   miscToSpell.forEach((key, value) => {
+//     events.SpellID.OnDamageEarly(value, (s, d, i, t, c) => {
+//       const p = s.GetCaster()
+//       const points = p.GetInt(key.toString(), 0)
+//       const damage = d.get() + (d.get() * (points * Random(0.020, 0.033)))
+//       d.set(damage) 
+//     })
+//   })
+// }
+
+// function setupMiscToSpell() {
+//   skillToSpell.forEach((v, index, arr) => {
+//     const s = GetSpellInfo(v[0][0])
+//     for (let i = 0; i < 3; i++) {
+//       let eff = s.GetEffect(i)
+//       if (eff.GetMiscValue() != 0)
+//         miscToSpell[eff.GetMiscValue()] = v[1]
+//     }
+//   })
+// }
+
+// on apply aura
 // - set [tag_dict_key]=true on player: sinister_strike=true, rogue=true
 
 // on remove aura:
@@ -9,28 +62,201 @@
 // - lookup tags for spell
 // - accumulate effectiveness for tags based on aura stacks (prefer dictionaries)
 
-const SpellTags = CreateDictionary<float, TSArray<TSString>>({})
+// const SpellTags = CreateDictionary<float, TSArray<TSString>>({})
 
-const EXAMPLE_TAGS: TSArray<TSString> = <TSArray<TSString>>([
-  'sinister-strike',
-  'offensive',
-  'rogue',
-]) 
+// const EXAMPLE_TAGS: TSArray<TSString> = <TSArray<TSString>>([
+//   'sinister-strike',
+//   'offensive',
+//   'rogue',
+// ]) 
 
-SpellTags.set(
-  GetIDTagUnique('spell-skills', 'SINISTER_STRIKE_FIRST'),
-  EXAMPLE_TAGS,
-)
+// SpellTags.set(
+//   GetIDTagUnique('spell-skills', 'SINISTER_STRIKE_FIRST'),
+//   EXAMPLE_TAGS,
+// )
+
+function Random (min: number = 1000000, max: number = 8999999): number {
+  return Math.floor(Math.random() * max) + min
+}
+
+function AtLeastZero (num: number): number {
+  return (num >= 0) ? num : 0
+}
+
+function RemoveSpellSkillTag (player: TSPlayer, id: string) {
+  const arr = player.GetJsonArray('spell-skills', new TSJsonArray())
+  for (let i = 0; i < arr.length; i++) {
+    const x = arr.GetString(i)
+    if (x === id)
+      arr.Remove(i)
+  }
+  player.SetJsonArray('spell-skills', arr)
+}
+
+function AddSpellSkillTag (player: TSPlayer, id: string) {
+  let alreadyExists = false
+  const arr = player.GetJsonArray('spell-skills', new TSJsonArray())
+  for (let i = 0; i < arr.length; i++) {
+    const x = arr.GetString(i)
+    if (x === id)
+      alreadyExists = true
+  }
+  if (!alreadyExists)
+    arr.PushString(id)
+  player.SetJsonArray('spell-skills', arr)
+}
 
 export function Main (events: TSEvents) {
-  // events.SpellID.OnApply
-  events.SpellID.OnDamageEarly(
-    GetIDTag('spell-skills', 'SINISTER_STRIKE'),
-    (spell, damage, info, type, isCrit, effectMask) => {
-      const caster = spell.GetCaster()
-      if (!caster.IsPlayer())
-        return
-      const player = caster.ToPlayer()
-    },
-  )
+  events.Player.OnSay((p, m) => {
+    if (m.get() === 'skills') {
+      p.AddTimer(1, 1, 0, o => {
+        const p = o.ToPlayer()
+        const arr = p.GetJsonArray('spell-skills', new TSJsonArray())
+        for (let i = 0; i < arr.length; i++)
+          p.SendBroadcastMessage(
+            `${arr.GetString(i)}: ${p.GetNumber(arr.GetString(i) + '_SKILL')}`
+          )
+      })
+    }
+  })
+
+  events.Player.OnLogin(p => {
+    p.SendBroadcastMessage(`${p.GetNumber('SINISTER_STRIKE_SKILL')}`)
+  })
+
+  // ALL
+  events.SpellID.OnApply(GetIDTag('spell-skills', 'All_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('ALL_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous + points)
+    p.SetNumber('ALL_SKILL', next)
+  })
+  events.SpellID.OnRemove(GetIDTag('spell-skills', 'All_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('ALL_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous - points)
+    p.SetNumber('ALL_SKILL', next)
+  })  
+
+  // MAGE
+  events.SpellID.OnApply(GetIDTag('spell-skills', 'Mage_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('MAGE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous + points)
+    p.SetNumber('MAGE_SKILL', next)
+  })
+  events.SpellID.OnRemove(GetIDTag('spell-skills', 'Mage_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('MAGE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous - points)
+    p.SetNumber('MAGE_SKILL', next)
+  })
+  
+  // ROGUE
+  events.SpellID.OnApply(GetIDTag('spell-skills', 'Rogue_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('ROGUE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous + points)
+    p.SetNumber('ROGUE_SKILL', next)
+  })
+  events.SpellID.OnRemove(GetIDTag('spell-skills', 'Rogue_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('ROGUE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous - points)
+    p.SetNumber('ROGUE_SKILL', next)
+  })
+
+  // FIRE
+  events.SpellID.OnApply(GetIDTag('spell-skills', 'Fire_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('FIRE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous + points)
+    p.SetNumber('FIRE_SKILL', next)
+  })
+  events.SpellID.OnRemove(GetIDTag('spell-skills', 'Fire_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('FIRE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous - points)
+    p.SetNumber('FIRE_SKILL', next)
+  })
+
+  // COMBAT
+  events.SpellID.OnApply(GetIDTag('spell-skills', 'Combat_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('COMBAT_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous + points)
+    p.SetNumber('COMBAT_SKILL', next)
+  })
+  events.SpellID.OnRemove(GetIDTag('spell-skills', 'Combat_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('COMBAT_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous - points)
+    p.SetNumber('COMBAT_SKILL', next)
+  })
+
+  // SINISTER_STRIKE
+  events.SpellID.OnApply(GetIDTag('spell-skills', 'SINISTER_STRIKE_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('SINISTER_STRIKE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous + points)
+    p.SetNumber('SINISTER_STRIKE_SKILL', next)
+    AddSpellSkillTag(p, 'SINISTER_STRIKE')
+  })
+  events.SpellID.OnRemove(GetIDTag('spell-skills', 'SINISTER_STRIKE_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('SINISTER_STRIKE_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous - points)
+    p.SetNumber('SINISTER_STRIKE_SKILL', next)
+    RemoveSpellSkillTag(p, 'SINISTER_STRIKE')
+  })
+  events.SpellID.OnDamageEarly(GetIDTag('spell-skills', 'SINISTER_STRIKE'), (spell, dmg) => {
+    const p = spell.GetCaster().ToPlayer()
+    const a: uint32 = AtLeastZero(p.GetNumber('SINISTER_STRIKE_SKILL'))
+    const b: uint32 = AtLeastZero(p.GetNumber('ALL_SKILL'))
+    const c: uint32 = AtLeastZero(p.GetNumber('ROGUE_SKILL'))
+    const d: uint32 = AtLeastZero(p.GetNumber('COMBAT_SKILL'))
+    const points = a + b + c + d
+    const damage = dmg.get() + (dmg.get() * (points * Random(0.1, 0.2)))
+    dmg.set(damage)
+  })
+
+  // FIREBALL
+  events.SpellID.OnApply(GetIDTag('spell-skills', 'FIREBALL_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('FIREBALL_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous + points)
+    p.SetNumber('FIREBALL_SKILL', next)
+    AddSpellSkillTag(p, 'FIREBALL')
+  })
+  events.SpellID.OnRemove(GetIDTag('spell-skills', 'FIREBALL_SKILL'), s => {
+    const p = s.GetCaster().ToPlayer()
+    const previous: uint32 = p.GetNumber('FIREBALL_SKILL')
+    const points: uint32 = s.GetSpellInfo().GetManaCost()
+    const next: uint32 = AtLeastZero(previous - points)
+    p.SetNumber('FIREBALL_SKILL', next)
+    RemoveSpellSkillTag(p, 'FIREBALL')
+  })
+  events.SpellID.OnDamageEarly(GetIDTag('spell-skills', 'FIREBALL'), (s, dmg) => {
+    const p = s.GetCaster().ToPlayer()
+    const a: uint32 = AtLeastZero(p.GetNumber('FIREBALL_SKILL'))
+    const b: uint32 = AtLeastZero(p.GetNumber('ALL_SKILL'))
+    const c: uint32 = AtLeastZero(p.GetNumber('FIRE_SKILL'))
+    const d: uint32 = AtLeastZero(p.GetNumber('MAGE_SKILL'))
+    const points = a + b + c + d
+    const damage = dmg.get() + (dmg.get() * (points * Random(0.05, 0.1)))
+    dmg.set(damage)
+  })
 }
