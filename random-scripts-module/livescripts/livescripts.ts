4a044c30d0703caf190b4c8de013b93e8f66c6f1
tester <connorkilbourne@gmail.com>
2022-06-19 21:55:05 -0400

    dump player inventory

--- random-scripts-module/livescripts/livescripts.ts
+++ random-scripts-module/livescripts/livescripts.ts
@@ -1,5 +1,7 @@
+import { dumpPlayerInventory } from "./dump-player-inventory"
 import { itemRandomEnchantVisual } from "./randomenchants"
 
 export function Main(events: TSEvents) {
     itemRandomEnchantVisual(events)
+    dumpPlayerInventory(events)
 }
