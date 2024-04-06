5f2be63c9cc5643fe368ddd1f0987e761fc97a6c
TesterDev <42619070+TesterWoWDev@users.noreply.github.com>
2024-04-06 12:13:16 -0400

    update addon

--- item-creation/addon/item-cache-request.ts
+++ item-creation/addon/item-cache-request.ts
@@ -1,19 +1,11 @@
-// ============================================================================
-//
-// - Item Cache Updater -
-//
-//   This file forces the client to update item information on hover, removing need to update the whole server whenever a item changes
-//
-// - External scripts -
-//   Livescripts: livescripts/item-cache
-//
-// ============================================================================
-
 import { itemCache } from "../shared/Messages";
-
+let lastItemID = "0"
 export function itemCacheRequest() {
     GameTooltip.HookScript("OnTooltipSetItem", (self) => {
         const itemID = self.GetItem()[1].split(':')[1]
-        new itemCache(Number(itemID)).write().Send()
+        if (itemID != lastItemID) {
+            new itemCache(Number(itemID)).write().Send()
+            lastItemID = itemID
+        }
     });
 }
